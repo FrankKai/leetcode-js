52836a71895ccaabf1fff1164df4257c992fa74b
FrankKai <gaokai20100801@gmail.com>
2020-03-28 10:29:59 +0800

    新增349题数组交集答案;

---
+++ 349.Intersection_of_Two_Arrays.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  /**
+   * 解法1：数组去重，最小长度数组，交集数组
+   * 性能：72 ms 50.27% 34.8MB 42.22%
+   */
+  var set1 = new Set(nums1);
+  var set2 = new Set(nums2);
+  // 数组去重
+  var set1Arr = Array.from(set1);
+  var set2Arr = Array.from(set2);
+  var minLength = Math.min(set1Arr.length, set2Arr.length);
+  // 找出最小长度数组，减少遍历次数
+  var iterateArr = minLength === set1Arr.length ? set1Arr : set2Arr;
+  // 定义交集数组
+  var crossArr = [];
+  iterateArr.forEach(item => {
+    if (set1Arr.indexOf(item) !== -1 && set2Arr.indexOf(item) !== -1) {
+      crossArr.push(item);
+    }
+  });
+  return crossArr;
+  /**
+   * 解法2：最小长度数组，交集数组，数组去重
+   * 性能：76 ms 34.72% 34 MB 88.89%
+   */
+  //   var minLength = Math.min(nums1.length, nums2.length);
+  //   // 找出最小长度数组，减少遍历次数
+  //   var iterateArr = minLength === nums1.length ? nums1 : nums2;
+  //   // 定义交集数组
+  //   var crossArr = [];
+  //   iterateArr.forEach(item => {
+  //     if (nums1.indexOf(item) !== -1 && nums2.indexOf(item) !== -1) {
+  //       crossArr.push(item);
+  //     }
+  //   });
+  //   var crossArrSet = Array.from(new Set(crossArr));
+  //   return crossArrSet;
+};
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # leetcode-js
+
 Leetcode's answers by javascript.
 
 ### easy
@@ -13,13 +14,15 @@ Leetcode's answers by javascript.
 - [26.Remove Duplicates from Sroted Array](/26.Remove_Duplicates_from_Sroted_Array.js)
 - [27.Remove Element](/27.Remove_Element.js)
 - [28.Implement strStr()](/28.Implement_strStr().js)
-- [35.Search_Insert_Position](/35.Search_Insert_Position.js)
+- [35.Search Insert Position](/35.Search_Insert_Position.js)
+- [349.Intersection of Two Arrays](/349.Intersection_of_Two_Arrays.js)
 
 ### medium
+
 - [94.Binary Tree Inorder Traversal](/94.Binary_Tree_Inorder_Traversal.js)
 - [144.Binary Tree Preorder Traversal](/144.Binary_Tree_Preorder_Traversal.js)
 - [622.Queue Circular](/622.Queue_Circular.js)
 
-
 ### difficult
+
 waiting...
