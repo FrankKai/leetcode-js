54ccf7449f15857eae05d546392d957a61519d36
FrankKai <gaokai20100801@gmail.com>
2020-03-30 17:10:18 +0800

    增加217 Set add和has解法;

--- 217.Contains_Duplicate.js
+++ 217.Contains_Duplicate.js
@@ -3,6 +3,21 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+  /**
+   * 解法一：对比Set和Array的长度
+   */
   var arrSet = new Set(nums);
   return arrSet.size !== nums.length;
+  /**
+   * 解法二：迭代Array，存储到Set中，存储前判断是否has
+   */
+  var arrSet = new Set();
+  for (var i = 0; i < nums.length; i++) {
+    if (!arrSet.has(nums[i])) {
+      arrSet.add(nums[i]);
+    } else {
+      return true;
+    }
+  }
+  return false;
 };
