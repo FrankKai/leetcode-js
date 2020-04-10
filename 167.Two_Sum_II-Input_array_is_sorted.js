8b52c1256adcd54d940f88ff8c08c55e201e6d80
FrankKai <gaokai20100801@gmail.com>
2020-04-10 16:15:30 +0800

    update 167 两数之和2 对撞指针解法

--- 167.Two_Sum_II-Input_array_is_sorted.js
+++ 167.Two_Sum_II-Input_array_is_sorted.js
@@ -17,4 +17,20 @@ var twoSum = function (numbers, target) {
       }
     }
   }
+  /**
+   * 解法2：对撞指针
+   * 性能：68ms 71.18% 35.2MB 76.60%
+   * 时间复杂度：O(n^2)
+   */
+  var left = 0;
+  var right = numbers.length - 1;
+  while (left < right) {
+    if (numbers[left] + numbers[right] === target) {
+      return [left + 1, right + 1];
+    } else if (numbers[left] + numbers[right] > target) {
+      right--;
+    } else {
+      left++;
+    }
+  }
 };
