b986689e74b05bcc6d7556089d52eabaebd0aa0a
FrankKai <gaokai20100801@gmail.com>
2020-04-10 22:37:31 +0800

    update 9 简便解法和对撞指针法

--- 9.Palindrome_Number.js
+++ 9.Palindrome_Number.js
@@ -2,19 +2,43 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    var strX = x + '';
-    var strArr = [];
-    var strLength = strX.length;
-    var isPalindrome = true;
-    for(var i=0; i<strLength; i++){
-        strArr.push(strX.charAt(i));
+var isPalindrome = function (x) {
+  /**
+   * 解法1
+   * 性能：344ms 46.1MB
+   */
+  var strX = x + "";
+  var strArr = [];
+  var strLength = strX.length;
+  var isPalindrome = true;
+  for (var i = 0; i < strLength; i++) {
+    strArr.push(strX.charAt(i));
+  }
+  var strArrReverse = strArr.map((e) => e).reverse();
+  strArr.forEach((e, i) => {
+    if (e !== strArrReverse[i]) {
+      isPalindrome = false;
     }
-    var strArrReverse = strArr.map((e)=>e).reverse();
-    strArr.forEach((e,i)=>{
-        if(e!==strArrReverse[i]){
-            isPalindrome = false;
-        }
-    })
-    return isPalindrome;
+  });
+  return isPalindrome;
+  /**
+   * 解法2：Array.prototype.reverse
+   * 性能：240ms 45.8MB
+   */
+  return `${x}` === `${x}`.split("").reverse().join("");
+  /**
+   * 解法3：对撞指针法
+   * 性能：244ms 45.5MB
+   */
+  var strArr = `${x}`.split("");
+  var i = 0;
+  var j = strArr.length - 1;
+  while (i < j) {
+    if (strArr[i] !== strArr[j]) {
+      return false;
+    }
+    i++;
+    j--;
+  }
+  return true;
 };
