3f06224af31e366cd9bcb4cf956a339c1a804eda
FrankKai <gaokai20100801@gmail.com>
2020-04-13 15:27:31 +0800

    update 27 移除元素 对撞指针法

--- 27.Remove_Element.js
+++ 27.Remove_Element.js
@@ -3,13 +3,34 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-  // 指针加递归
+var removeElement = function (nums, val) {
+  /**
+   * 解法1：指针加递归
+   * 性能：68ms 33.7MB
+   */
   var i = 0;
   for (; i < nums.length; ) {
     if (nums[i] === val) {
       nums.splice(i, 1);
       removeElement(nums, val);
+    } else {
+      i++;
+    }
+  }
+  return nums.length;
+  /**
+   * 解法2：对撞指针
+   * 性能：64ms 33.9MB
+   */
+  var i = 0;
+  var j = nums.length - 1;
+  while (i <= j) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      j--;
+    } else if (nums[j] === val) {
+      nums.splice(j, 1);
+      j--;
     } else {
       i++;
     }
