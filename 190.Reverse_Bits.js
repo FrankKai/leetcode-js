9db961e18e44b95942e3224339387be99411d816
FrankKai <gaokai20100801@gmail.com>
2020-04-14 12:00:21 +0800

    update 190 answer 2

--- 190.Reverse_Bits.js
+++ 190.Reverse_Bits.js
@@ -5,14 +5,14 @@
 var reverseBits = function (n) {
   /**
    * 解法1：转数组后对撞交换位置
+   * 性能：76ms 35.8MB
    * 思路：
    * 十进制转二进制：toString(2)
    * 32位空位补0：padStart(32,0)
    * 反转算法：对撞指针法
    * 二进制转十进制：parseInt(,2)
-   * 性能：76ms 35.8MB
    */
-  let arr = `${n.toString(2).padStart(32, 0)}`.split("");
+  let arr = n.toString(2).padStart(32, 0).split("");
   let head = 0;
   let tail = arr.length - 1;
   while (head < tail) {
@@ -27,4 +27,12 @@ var reverseBits = function (n) {
   }
   let result = parseInt(arr.join(""), 2);
   return result;
+  /**
+   * 解法2：转数组后交换位置
+   * 性能：120ms 36MB
+   */
+  return parseInt(
+    n.toString(2).padStart(32, 0).split("").reverse().join(""),
+    2
+  );
 };
