46e457f2ec3a042fd6535a0a4454f95854367aba
FrankKai <gaokai20100801@gmail.com>
2020-04-15 23:31:58 +0800

    update 228 stackToResult函数

--- 228.Summary_Ranges.js
+++ 228.Summary_Ranges.js
@@ -5,7 +5,7 @@
 var summaryRanges = function (nums) {
   /**
    * 解法1：排序 + 栈
-   * 性能：64ms 33.7MB
+   * 性能：52ms 33.7MB
    */
   nums.sort((a, b) => a - b);
   let stack = [];
@@ -14,21 +14,19 @@ var summaryRanges = function (nums) {
     if (stack.length === 0 || nums[i] - stack[stack.length - 1] === 1) {
       stack.push(nums[i]);
     } else {
-      if (stack.length > 1) {
-        result.push(`${stack[0]}->${stack[stack.length - 1]}`);
-      } else {
-        result.push(`${stack[0]}`);
-      }
-
+      stackToResult();
       stack = [];
       stack.push(nums[i]);
     }
     if (i === nums.length - 1) {
-      if (stack.length > 1) {
-        result.push(`${stack[0]}->${stack[stack.length - 1]}`);
-      } else {
-        result.push(`${stack[0]}`);
-      }
+      stackToResult();
+    }
+  }
+  function stackToResult() {
+    if (stack.length > 1) {
+      result.push(`${stack[0]}->${stack[stack.length - 1]}`);
+    } else {
+      result.push(`${stack[0]}`);
     }
   }
   return result;
