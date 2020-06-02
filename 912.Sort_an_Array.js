830836beb0a200832644a78d82d2fb4c525e432e
FrankKai <gaokai20100801@gmail.com>
2020-06-02 22:16:48 +0800

    update 912.排序数组 插入排序(辅助数组版,修改自身版)

--- 912.Sort_an_Array.js
+++ 912.Sort_an_Array.js
@@ -54,4 +54,45 @@ var sortArray = function (nums) {
     }
   }
   return nums;
+  /**
+   * 解法5：插入排序
+   * 思路：已排序区间和未排序区间。取出未排序区间的第一项，在已排序区间上找到自己的位置,一般来说是找foo<x<bar，将x插入foo和bar之间，或者是x<bar插入头部。
+   * 时间复杂度: O(1)
+   * 性能：2008ms 43.9MB
+   * 关键点：插入到指定位置后立即停止在已排序数组中查找
+   * */
+  const sorted = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    // j = i - 1; 也行
+    for (let j = sorted.length - 1; j >= 0; j--) {
+      if (nums[i] < sorted[j]) {
+        if (j === 0) {
+          sorted.splice(j, 0, nums[i]);
+        }
+      } else if (nums[i] >= sorted[j]) {
+        sorted.splice(j + 1, 0, nums[i]);
+        j = -1; // 这里很关键，插入到指定位置后立即停止在已排序数组中查找
+      }
+    }
+  }
+  return sorted;
+  /**
+   * 解法5优化版：插入排序（不借助辅助数组）
+   * 性能：2372ms 42.5MB
+   * 思路：插入splice(j/j+1, 0), 删除splice(i, 1)[0]
+   * 需要注意的是: splice()返回的是一个数组，例如[1]
+   */
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (nums[i] < nums[j]) {
+        if (j === 0) {
+          nums.splice(j, 0, nums.splice(i, 1)[0]);
+        }
+      } else if (nums[i] >= nums[j]) {
+        nums.splice(j + 1, 0, nums.splice(i, 1)[0]);
+        j = -1;
+      }
+    }
+  }
+  return nums;
 };
