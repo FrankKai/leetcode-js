1d2345316710aee50a165d28facb830a44e228e2
FrankKai <gaokai20100801@gmail.com>
2020-06-02 15:35:35 +0800

    update 912.排序数组 增加冒泡排序解法;

--- 912.Sort_an_Array.js
+++ 912.Sort_an_Array.js
@@ -18,4 +18,19 @@ var sortArray = function (nums) {
     result.push(del);
   }
   return result;
+  /**
+   * 解法3：冒泡排序
+   * 思路：外层每次循环都是不断将最大值置于尾部,最小值像气泡一样向前冒出
+   * 性能：4704ms 39.4MB
+   */
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - 1 - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        const temp = nums[j];
+        nums[j] = nums[j + 1];
+        nums[j + 1] = temp;
+      }
+    }
+  }
+  return nums;
 };
