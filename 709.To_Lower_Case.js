c0785b71bc24a689dc887c76cd9bedb5ec052289
FrankKai <gaokai20100801@gmail.com>
2020-06-11 17:37:56 +0800

    update 709:转换成最小字母 TypeScript版

--- 709.To_Lower_Case.js
+++ 709.To_Lower_Case.js
@@ -28,3 +28,19 @@ var toLowerCase = function (str) {
    */
   return str.toLowerCase();
 };
+
+// TypeScript版
+function toLowerCase(str: string): string {
+  const upperRange = { min: 65, max: 90 };
+  const lowerArr = [];
+  for (let i = 0; i < str.length; i++) {
+    const code = str.charCodeAt(i);
+    if (code >= upperRange.min && code <= upperRange.max) {
+      lowerArr.push(String.fromCharCode(code + 32));
+    } else {
+      lowerArr.push(str[i]);
+    }
+  }
+  const result = lowerArr.join("");
+  return result;
+}
