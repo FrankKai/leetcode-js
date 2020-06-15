dacd4470bce9349b6b6d655dfe00647bc4edda2c
FrankKai <gaokai20100801@gmail.com>
2020-06-15 21:38:32 +0800

    update 237 更新删除链表节点 释放残留节点空间;

--- 237.Delete_Node_in_a_Linked_List.js
+++ 237.Delete_Node_in_a_Linked_List.js
@@ -14,13 +14,18 @@ var deleteNode = function (node) {
    * 思路：
    * 1.被删除节点val获取下一个节点的val
    * 2.被删除节点next指向下一个节点的next
+   * 3.清空残留的下一个节点
    *
    * 假设删除5
    * 4 -> 5 -> 1 -> 9
    *
    *           1----↓
    * 4 -> 1 ------> 9
+   *           1----> null
+   * 4 -> 1 ------> 9
    */
+  let next = node.next;
   node.val = node.next.val;
   node.next = node.next.next;
+  next = null;
 };
