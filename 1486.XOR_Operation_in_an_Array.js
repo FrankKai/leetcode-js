519e2e407ddd04fadb17ac7ea03e77c94fd62ef0
FrankKai <gaokai20100801@gmail.com>
2020-06-22 17:24:38 +0800

    更新1486.数组异或操作解法

--- 1486.XOR_Operation_in_an_Array.js
+++ 1486.XOR_Operation_in_an_Array.js
@@ -7,10 +7,21 @@ var xorOperation = function (n, start) {
   /**
    * 异或：a，b两个值不同时，异或为1；a，b两个值相同时，异或为0
    */
+  /**
+   * 解法1：辅助数组
+   */
   const nums = [];
   for (let i = 0; i <= n - 1; i++) {
     nums.push(start + 2 * i);
   }
   const result = nums.reduce((acc, cur) => acc ^ cur);
+  return result;
+  /**
+   * 解法2：一次遍历
+   */
+  let result = 0;
+  for (let i = 0; i <= n - 1; i++) {
+    result = result ^ (start + 2 * i);
+  }
   return result;
 };
