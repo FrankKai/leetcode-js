95a2ed42e74053612a24611fa62fc40efa91efe9
FrankKai <gaokai20100801@gmail.com>
2020-08-06 21:30:36 +0800

    更新205.同构字符串;哈希映射题解

--- 205.Isomorphic_Strings.js
+++ 205.Isomorphic_Strings.js
@@ -4,6 +4,11 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
+  /**
+   * 解法1：构造字符串
+   * egg=>122 add =>122
+   * foo=>122 bar=>123
+   */
   const markS = generatePositionMark(s);
   const markT = generatePositionMark(t);
   const result = markS === markT;
@@ -24,4 +29,21 @@ var isIsomorphic = function (s, t) {
     const joinStr = markArr.join("");
     return joinStr;
   }
+  /**
+   * 解法2：HashMap
+   */
+  const mapS = new Map(),
+    mapT = new Map();
+  let i = 0;
+  let result = true;
+  while (i < s.length) {
+    if (!mapS.has(s[i])) mapS.set(s[i], t[i]);
+    if (!mapT.has(t[i])) mapT.set(t[i], s[i]);
+    if (mapS.get(s[i]) !== t[i] || mapT.get(t[i]) !== s[i]) {
+      result = false;
+      break;
+    }
+    i++;
+  }
+  return result;
 };
