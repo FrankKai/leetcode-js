9ab683ae51d757acbc9115f157834ca6d24ada25
FrankKai <gaokai20100801@gmail.com>
2020-08-06 22:18:44 +0800

    更新599.两个列表的最小索引总和;解法2:哈希映射;

--- 599.Minimum_Index_Sum_of_Two_Lists.js
+++ 599.Minimum_Index_Sum_of_Two_Lists.js
@@ -4,6 +4,9 @@
  * @return {string[]}
  */
 var findRestaurant = function (list1, list2) {
+  /**
+   * 解法1: 数组统计法
+   */
   const commonLoves = [];
   for (let i = 0; i < list1.length; i++) {
     const list2Index = list2.indexOf(list1[i]);
@@ -21,5 +24,29 @@ var findRestaurant = function (list1, list2) {
     }
     return acc;
   }, []);
+  return result;
+  /**
+   * 解法2: 哈希集合统计法
+   */
+  const map1 = new Map();
+  const map2 = new Map();
+  for (let i = 0; i < list1.length; i++) {
+    map1.set(list1[i], i);
+  }
+  for (let j = 0; j < list2.length; j++) {
+    if (map1.has(list2[j])) {
+      map2.set(list2[j], map1.get(list2[j]) + j);
+    }
+  }
+  let min = -Infinity;
+  let result = [];
+  for (const [key, value] of map2) {
+    if (min === -Infinity || min > value) {
+      min = value;
+      result = [key];
+    } else if (min === value) {
+      result.push(key);
+    }
+  }
   return result;
 };
