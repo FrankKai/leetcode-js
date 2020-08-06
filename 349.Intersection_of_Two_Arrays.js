6050307631d137e76e8e5b18b39b87d7bbabc763
FrankKai <gaokai20100801@gmail.com>
2020-08-06 12:07:55 +0800

    更新349.两个数组的交集题解;

--- 349.Intersection_of_Two_Arrays.js
+++ 349.Intersection_of_Two_Arrays.js
@@ -3,7 +3,7 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-var intersection = function(nums1, nums2) {
+var intersection = function (nums1, nums2) {
   /**
    * 解法1：数组去重，最小长度数组，交集数组
    * 性能：72 ms 50.27% 34.8MB 42.22%
@@ -18,7 +18,7 @@ var intersection = function(nums1, nums2) {
   var iterateArr = minLength === set1Arr.length ? set1Arr : set2Arr;
   // 定义交集数组
   var crossArr = [];
-  iterateArr.forEach(item => {
+  iterateArr.forEach((item) => {
     if (set1Arr.indexOf(item) !== -1 && set2Arr.indexOf(item) !== -1) {
       crossArr.push(item);
     }
@@ -28,16 +28,31 @@ var intersection = function(nums1, nums2) {
    * 解法2：最小长度数组，交集数组，数组去重
    * 性能：76 ms 34.72% 34 MB 88.89%
    */
-  //   var minLength = Math.min(nums1.length, nums2.length);
-  //   // 找出最小长度数组，减少遍历次数
-  //   var iterateArr = minLength === nums1.length ? nums1 : nums2;
-  //   // 定义交集数组
-  //   var crossArr = [];
-  //   iterateArr.forEach(item => {
-  //     if (nums1.indexOf(item) !== -1 && nums2.indexOf(item) !== -1) {
-  //       crossArr.push(item);
-  //     }
-  //   });
-  //   var crossArrSet = Array.from(new Set(crossArr));
-  //   return crossArrSet;
+  var minLength = Math.min(nums1.length, nums2.length);
+  // 找出最小长度数组，减少遍历次数
+  var iterateArr = minLength === nums1.length ? nums1 : nums2;
+  // 定义交集数组
+  var crossArr = [];
+  iterateArr.forEach((item) => {
+    if (nums1.indexOf(item) !== -1 && nums2.indexOf(item) !== -1) {
+      crossArr.push(item);
+    }
+  });
+  var crossArrSet = Array.from(new Set(crossArr));
+  return crossArrSet;
+  /**
+   * 解法3
+   */
+
+  const result = [];
+  while (nums1.length) {
+    const pop = nums1.pop();
+    if (result.indexOf(pop) !== -1) {
+      continue;
+    }
+    if (nums2.indexOf(pop) !== -1) {
+      result.push(pop);
+    }
+  }
+  return result;
 };
