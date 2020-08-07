99f5c7e920d15b1251754cb3b1a5f19f5fce1ee4
FrankKai <gaokai20100801@gmail.com>
2020-08-07 10:25:42 +0800

    更新387.字符串中的第一个唯一字符;

--- 387.First_Unique_Character_in_a_String.js
+++ 387.First_Unique_Character_in_a_String.js
@@ -3,6 +3,9 @@
  * @return {number}
  */
 var firstUniqChar = function (s) {
+  /**
+   * 解法1:数组
+   */
   const obj = {};
   for (let i = 0; i < s.length; i++) {
     const char = s[i];
@@ -15,4 +18,26 @@ var firstUniqChar = function (s) {
   const onlyOne = Object.values(obj).filter((char) => char.count === 1);
   const result = onlyOne.length >= 1 ? onlyOne[0].firstIndex : -1;
   return result;
+  /**
+   * 解法2：HashMap
+   * 注意：
+   * Map设置的key，会按照设置顺序进行先后排序
+   * Object设置的key没有这个特性
+   */
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    const char = s[i];
+    map.set(
+      char,
+      map.get(char)
+        ? { index: i, count: map.get(char).count + 1 }
+        : { index: i, count: 1 }
+    );
+  }
+  for (const [key, obj] of map) {
+    if (obj.count === 1) {
+      return obj.index;
+    }
+  }
+  return -1;
 };
