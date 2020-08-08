66e8b6b3ad07f0756beb98fce39d033b9ba677b8
FrankKai <gaokai20100801@gmail.com>
2020-08-08 15:20:02 +0800

    更新771.宝石与石头;题解2HashSet;

--- 771.Jewels_and_Stones.js
+++ 771.Jewels_and_Stones.js
@@ -4,9 +4,21 @@
  * @return {number}
  */
 var numJewelsInStones = function (J, S) {
+  /**
+   * 解法1: 数组
+   */
   let count = 0;
   for (const char of S) {
     if (J.indexOf(char) !== -1) count++;
+  }
+  return count;
+  /**
+   * 解法2: HashSet
+   */
+  let count = 0;
+  const JSet = new Set(J);
+  for (const char of S) {
+    if (JSet.has(char)) count++;
   }
   return count;
 };
