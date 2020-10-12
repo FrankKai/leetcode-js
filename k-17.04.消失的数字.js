844e1e3d097e286d6073b160e3c74431bcf99b24
FrankKai <gaokai20100801@gmail.com>
2020-10-12 11:04:25 +0800

    更新17.04.消失的数字题解

--- k-17.04.消失的数字.js
+++ k-17.04.消失的数字.js
@@ -3,12 +3,23 @@
  * @return {number}
  */
 var missingNumber = function (nums) {
+  /**解法1 */
   const n = nums.length;
   let sumN = 0;
   for (let i = 0; i <= n; i++) {
     sumN += i;
   }
   const sumNums = nums.reduce((acc, cur) => acc + cur);
+  const result = sumN - sumNums;
+  return result;
+  /**解法2 */
+  let sumN = 0;
+  let sumNums = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sumN += i;
+    sumNums += nums[i];
+  }
+  sumN += nums.length;
   const result = sumN - sumNums;
   return result;
 };
