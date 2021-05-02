24a99762bba7e9f0cad21a5f791940ba41fc9bc1
FrankKai <gaokai20100801@gmail.com>
2021-05-02 11:55:21 +0800

    update 303 pre sum

--- 303.Range_Sum_Query_Immutable.js
+++ 303.Range_Sum_Query_Immutable.js
@@ -1,3 +1,4 @@
+// 暴力解法(查询时的时间复杂度：O(n))
 /**
  * @param {number[]} nums
  */
@@ -23,3 +24,29 @@ NumArray.prototype.sumRange = function (i, j) {
  * var obj = new NumArray(nums)
  * var param_1 = obj.sumRange(i,j)
  */
+
+// 前缀和解法(查询时的时间复杂度：O(1))
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function (nums) {
+  this.preSum = [0];
+  for (let i = 0; i < nums.length; i++) {
+      this.preSum[i + 1] = this.preSum[i] + nums[i]
+  }
+};
+
+/** 
+* @param {number} left 
+* @param {number} right
+* @return {number}
+*/
+NumArray.prototype.sumRange = function (left, right) {
+  return this.preSum[right + 1] - this.preSum[left]
+};
+
+/**
+* Your NumArray object will be instantiated and called as such:
+* var obj = new NumArray(nums)
+* var param_1 = obj.sumRange(left,right)
+*/
