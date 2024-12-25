0dfb99d179a00a63ee707ad592624787712ab7ca
FrankKai <gaokai20100801@gmail.com>
2024-12-25 18:11:20 +0800

    Update 26.Remove_Duplicates_from_Sroted_Array.js

--- easy/26.Remove_Duplicates_from_Sroted_Array.js
+++ easy/26.Remove_Duplicates_from_Sroted_Array.js
@@ -3,6 +3,15 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+  // 前提是升序排列，因此只要和i位置数字不同，就记录到++i的位置
+  let i = 0;
+  for(let j = 1;j<nums.length;j++){
+      if(nums[j]!==nums[i]){
+          nums[++i] = nums[j]
+      }
+  }
+  return i+1;
+
   // 双指针法
   // 慢指针i，快指针j
   var i = 0;
